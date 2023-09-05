9a4a482b9de5b24d8959bb17a1e1c1c6b5551621
Raj Singh <rajsingh.mailto@gmail.com>
2023-09-05 22:20:49 +0530

    AC Functinality added

--- script.js
+++ script.js
@@ -11,17 +11,10 @@ function divide(a,b){
     if(b==0) return "OOPS";
     return a/b;
 }
-let operand1;
-let operand2;
-let operator;
-function operate(){
-    operand1 = parseInt(prompt("Enter Value 1"));
-    operator = prompt("Enter the Operator:('+', '-', '*','/')"  );
-    operand2 = parseInt(prompt("Enter value 2"));
-    let result;
-    if(operator == "+") result = add(operand1,operand2);
-    if(operator == "-") result = subtract(operand1,operand2);
-    if(operator == "*") result = multiply(operand1,operand2);
-    if(operator == "/") result = divide(operand1,operand2);
-    alert(result);
-}
+const prevSrc = document.querySelector("#prev-screen");
+const currSrc = document.querySelector("#curr-screen");
+const acbtn = document.querySelector("#ac-btn");
+acbtn.addEventListener('click',()=>{
+    prevSrc.innerHTML = "";
+    currSrc.innerHTML = "0";
+});
